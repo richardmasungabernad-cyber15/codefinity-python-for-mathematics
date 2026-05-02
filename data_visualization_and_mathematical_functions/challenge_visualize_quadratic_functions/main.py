fb66eb239cf04c516280591e6d569e34eb6c76f8
richardmasungabernad-cyber15 <>
2026-05-02 12:21:30 +0000

    Challenge: Visualize Quadratic Functions

---
+++ data_visualization_and_mathematical_functions/challenge_visualize_quadratic_functions/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_quadratic(a, b, c):
+    # Write your code here
+    x = np.linspace(-10,10,400)
+    y = a*(x**2) + b*x + c
+    
+    plt.plot(x,y)
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(f"Graph of the quadratic function y ={a}x^2 + {b}x + {c}")
+    plt.grid(True)
+    plt.show()
+# Test with a = 1, b = -2, c = 1
+plot_quadratic(1, -2, 1)
